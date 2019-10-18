1d9bc28b20b7d4b0a27fc092bad612dc23d8c813
kydyrbekova <a.kydyrbekova@gmail.com>
2019-10-18 23:40:07 +0600

    updating raid1a.go_3

---
+++ raid1a.go
@@ -0,0 +1,37 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DrawBase(x int) {
+	for i := 0; i < x; i++ {
+		if i == 0 || i == x-1 {
+			z01.PrintRune(111)
+		} else if i < x-1 {
+			z01.PrintRune(45)
+		}
+	}
+}
+
+func Raid1a(x, y int) {
+	side := 0
+	if x > 1 {
+		side = 2
+	} else {
+		side = 1
+	}
+	DrawBase(x)
+	z01.PrintRune(rune(10))
+	if y > 1 {
+		for l := 0; l < y-2; l++ {
+			for j := 0; j < side; j++ {
+				z01.PrintRune(124)
+				for k := 0; k < x-2; k++ {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune(rune(10))
+		}
+		DrawBase(x)
+		z01.PrintRune(rune(10))
+	}
+}
